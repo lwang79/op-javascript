5360ee23b98126566cc45e4777f4067383d23177
lwang79 <lucy.wang7@gmail.com>
2024-10-01 22:49:56 +1000

    lw commits

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+  if (times < 0) return "ERROR";
+  let m = "";
+  for (let rep = 0; rep < times; rep++) {
+    m+=string;
+  };
+  return m;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
+
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let len = string.length;
+  let char = "";
+  let word = "";
+  let c = string.length;
+  for (i = 0; i < len; i++) {
+    char = string.substring(c,c-1);
+    c--;
+    word+=char;
+  };
+  return word;
 };
 
 // Do not edit below this line
